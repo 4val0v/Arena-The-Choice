38ba7a11228c3bdac6d7ba876c04de5ecd13c54d
afrokick <afrokick@mail.by>
2015-01-24 17:48:30 +0300

    Add total value for par

--- Assets/Scripts/Network/PlayerData.cs
+++ Assets/Scripts/Network/PlayerData.cs
@@ -9,4 +9,65 @@ public class PlayerData
     public string Name { get; set; }
     public CharacterClass Class { get; set; }
     public readonly List<int> EquippedItems = new List<int>();
+
+    public CharacterData BaseData { get { return CharacterDataProviders.GetBaseData(Class); } }
+
+    public float Def
+    {
+        get
+        {
+            var par = BaseData.BaseDef;
+            foreach (var item in EquippedItems)
+            {
+                par += ItemsProvider.GetItem(item).Defense;
+            }
+
+            return par;
+        }
+    }
+
+    public float Dmg
+    {
+        get
+        {
+            var par = BaseData.BaseDmg;
+            foreach (var item in EquippedItems)
+            {
+                par += ItemsProvider.GetItem(item).Dmg;
+            }
+
+            return par;
+        }
+    }
+
+    public float AttackSpeed
+    {
+        get
+        {
+            var par = BaseData.BaseAttackSpeed;
+
+            foreach (var item in EquippedItems)
+            {
+                par += ItemsProvider.GetItem(item).AttackSpeed;
+            }
+
+            return par;
+        }
+    }
+
+    public float Accuracy
+    {
+        get
+        {
+            var par = BaseData.BaseAccuracy;
+
+            foreach (var item in EquippedItems)
+            {
+                par += ItemsProvider.GetItem(item).Accuracy;
+            }
+
+            return par;
+        }
+    }
+
 }
