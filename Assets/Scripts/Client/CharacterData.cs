07902f674e2230e27f6962b931b3e174f6fedcf5
Alexey Gulev <alexeygulev@AGulev.local>
2015-01-24 18:22:02 +0300

    fix character data

--- Assets/Scripts/Client/CharacterData.cs
+++ Assets/Scripts/Client/CharacterData.cs
@@ -22,31 +22,31 @@ public static class CharacterDataProviders
         _characters.Add(CharacterClass.Man, new CharacterData
         {
             Class = CharacterClass.Man,
-            BaseHp = 100,
-            BaseDmg = 20,
-            BaseAttackSpeed = 10,
-            BaseDef = 10,
-            BaseAccuracy = 0.6f,
+            BaseHp = 3000,
+            BaseDmg = 0,
+            BaseAttackSpeed = 0,
+            BaseDef = 0,
+            BaseAccuracy = 0,
         });
 
         _characters.Add(CharacterClass.Woman, new CharacterData
         {
             Class = CharacterClass.Woman,
-            BaseHp = 100,
-            BaseDmg = 10,
-            BaseAttackSpeed = 20,
-            BaseDef = 10,
-            BaseAccuracy = 0.6f,
+			BaseHp = 3000,
+			BaseDmg = 0,
+			BaseAttackSpeed = 0,
+			BaseDef = 0,
+			BaseAccuracy = 0,
         });
 
         _characters.Add(CharacterClass.Orc, new CharacterData
         {
             Class = CharacterClass.Orc,
-            BaseHp = 100,
-            BaseDmg = 15,
-            BaseAttackSpeed = 10,
-            BaseDef = 15,
-            BaseAccuracy = 0.6f,
+			BaseHp = 3000,
+			BaseDmg = 0,
+			BaseAttackSpeed = 0,
+			BaseDef = 0,
+			BaseAccuracy = 0,
         });
     }
 
