a9312e5f78cb9ced4060276e757b77e724b995ba
Alexey Gulev <alexeygulev@AGulev.local>
2015-01-24 15:56:56 +0300

    add123

--- Assets/Scripts/Client/SkillBtn.cs
+++ Assets/Scripts/Client/SkillBtn.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class SkillBtn : MonoBehaviour {
 
@@ -9,7 +10,17 @@ public class SkillBtn : MonoBehaviour {
 	
 	void Update () 
 	{
-	
+		if (_cdwn > 0)
+		{
+			_cdwn -= Time.deltaTime;
+			_text.text = ""+((int)_cdwn+1);
+		}
+		else if (_cdwn != -1)
+		{
+			_cdwn = -1;
+			_text.text = "";
+			_btn.interactable = true;
+		}
 	}
 	
 	public void SetAbillity(AbilityData abbility)
@@ -17,6 +28,26 @@ public class SkillBtn : MonoBehaviour {
 		_abl = abbility;
 	}
 
+	void OnEnable()
+	{
+		_text.text = "";
+		_cdwn = -1;
+	}
+
+	void StartCouldown()
+	{
+		_btn.interactable = false;
+		_cdwn = _abl.Cooldown;
+	}
+
 	private AbilityData _abl;
+
+	private float _cdwn;
+
+	[SerializeField]
+	private Button _btn;
+
+	[SerializeField]
+	private Text _text;
 
 }
