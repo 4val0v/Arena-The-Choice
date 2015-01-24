5c012548e88db4105f24998a7c348b84d3da05a9
afrokick <afrokick@mail.by>
2015-01-24 11:38:40 +0300

    Add top bar view and prefab

---
+++ Assets/Scripts/Client/TopBar.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TopBar : MonoBehaviour
+{
+    public Text PlayerName;
+    public Text EnemyName;
+
+    public Slider PlayerHp;
+    public Slider EnemyHp;
+
+    public void UpdatePlayerName(string name)
+    {
+        PlayerName.text = name;
+    }
+
+    public void UpdateEnemyHp(string name)
+    {
+        EnemyName.text = name;
+    }
+
+    public void SetPlayerHp(float percent)
+    {
+        PlayerHp.value = percent;
+    }
+
+    public void SetEnemyHp(float percent)
+    {
+        EnemyHp.value = percent;
+    }
+}
