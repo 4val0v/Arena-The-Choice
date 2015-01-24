f27ecd37fa78f21eb7f1216761f31c2da053f87f
Alexey Gulev <alexeygulev@AGulev.local>
2015-01-24 15:35:09 +0300

    fix abilyty btn

---
+++ Assets/Scripts/Client/SkillBtn.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkillBtn : MonoBehaviour {
+
+	void Start () 
+	{
+	}
+	
+	void Update () 
+	{
+	
+	}
+	
+	public void SetAbillity(AbilityData abbility)
+	{
+		_abl = abbility;
+	}
+
+	private AbilityData _abl;
+
+}
