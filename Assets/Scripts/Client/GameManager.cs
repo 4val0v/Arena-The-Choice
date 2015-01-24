09c15967726b00b6b215be5f58b7654f45851f7f
Alexey Gulev <alexeygulev@AGulev.local>
2015-01-24 09:59:53 +0300

    main and select character scene

---
+++ Assets/Scripts/Client/GameManager.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager:MonoBehaviour
+{
+	void Awake()
+	{
+		_client = PunNetClient.Instance;
+		_client.OnStatusChanged += HandleOnStatusChanged;
+		_client.OnEnemyNameUpdated += HandleOnEnemyNameUpdated;
+		_client.OnEnemyClassUpdated += HandleOnEnemyClassUpdated;
+		_client.OnFirstPlayerReceived += HandleOnFirstPlayerReceived;
+		_client.OnStepItemsReceived += HandleOnStepItemsReceived;
+		_client.OnItemEquipped += HandleOnItemEquipped;
+		_client.OnGameStarted += HandleOnGameStarted;
+		_client.OnGameFinished += HandleOnGameFinished;
+	}
+
+	void HandleOnGameFinished (int playrWinID)
+	{
+		//onPlayerWin;
+	}
+
+	void HandleOnGameStarted ()
+	{
+		//fight
+	}
+
+	void HandleOnItemEquipped (int playerId, int itemId)
+	{
+		//get item
+	}
+
+	void HandleOnStepItemsReceived (EquipStep step, System.Collections.Generic.IEnumerable<int> items)
+	{
+		//create screen change item
+	}
+
+	void HandleOnFirstPlayerReceived (int playerId)
+	{
+		//id of first 
+	}
+
+	void HandleOnEnemyClassUpdated (CharacterClass classId)
+	{
+		//change enemy character 
+	}
+
+	void HandleOnEnemyNameUpdated (string name)
+	{
+		//update enemy name;
+	}
+
+	void HandleOnStatusChanged (NetStatus status)
+	{
+		switch (status) {
+		case NetStatus.Connected:
+			// first screen;
+			break;
+		case NetStatus.Disconnected:
+			//connect;to fist screen
+			break;
+		case NetStatus.ConnectingToBattle:
+			//mod
+			break;
+		case NetStatus.ConnectedToBattle:
+			break;
+		default:
+						break;
+		}
+	}
+
+	private INetClient _client;
+}
---
+++ Assets/Scripts/Client/SelectCharacter.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class SelectCharacter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public int CharacterIndex 
+	{
+		set;
+		get;
+	}
+
+	private void GetIt()
+	{
+		//Character was selected
+	}
+
+	[SerializeField]
+	private List<Button> _buttons;
+}
