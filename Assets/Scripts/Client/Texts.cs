9312f1792d8e4cce0ce03f46d561a33bd5c6f50f
Alexey Gulev <alexeygulev@AGulev.local>
2015-01-25 04:51:43 +0300

    Add waiting Screen

--- Assets/Scripts/Client/Texts.cs
+++ Assets/Scripts/Client/Texts.cs
@@ -1,6 +1,6 @@
 public class Texts
 {
-	public const string PLAYER_WAIT = "Wait you opponent";
+	public const string PLAYER_WAIT = "Waiting your opponent";
 	public const string CONNECTING = "Connecting";
 
     public const string WINNER_TITLE = "You win!";
