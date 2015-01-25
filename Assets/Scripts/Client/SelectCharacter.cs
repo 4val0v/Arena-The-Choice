558496fa57b08b7f374f590e46447f066c0c5bb0
Alexey Gulev <alexeygulev@AGulev.local>
2015-01-25 03:58:36 +0300

    Select Character Screen

--- Assets/Scripts/Client/SelectCharacter.cs
+++ Assets/Scripts/Client/SelectCharacter.cs
@@ -6,20 +6,36 @@ using System;
 
 public class SelectCharacter : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void OnEnable()
+	{
+		_characterIndex = -1;
 	}
 
 	public int CharacterIndex 
 	{
-		set;
-		get;
+		set
+		{
+			if (_characterIndex == -1)
+			{
+				_getBtn.interactable = true;
+			}
+			_characterIndex = value;
+			for (int i = 0; i < _buttons.Count; i++) 
+			{
+				if (i == value)
+				{
+					_buttons[i].image.sprite = _spriteOfSelectedItem;
+				}
+				else
+				{
+					_buttons[i].image.sprite = _spriteOfstandartPanel;
+				}
+			}
+		}
+		get
+		{
+			return _characterIndex;
+		}
 	}
 
 	public void GetIt()
@@ -32,6 +48,13 @@ public class SelectCharacter : MonoBehaviour {
 
 	[SerializeField]
 	private List<Button> _buttons;
-
+	private int _characterIndex = -1;
 	public Action<int> OnCharacterSelected;
+
+	[SerializeField]
+	private Button _getBtn;
+	[SerializeField]
+	private Sprite _spriteOfSelectedItem;
+	[SerializeField]
+	private Sprite _spriteOfstandartPanel;
 }
